1297273def21dd79bcf10af3ac4250e7e86e54e9
amal120 <amal_zine_el_abidine@yahoo.fr>
2016-04-16 18:17:58 +0000

    Update DocInterface.java

--- app/src/main/java/com/example/dell/mediatheque/DocInterface.java
+++ app/src/main/java/com/example/dell/mediatheque/DocInterface.java
@@ -13,79 +13,12 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class DocInterface extends AppCompatActivity {
-    ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document);
-        listView = (ListView) findViewById(R.id.list);
-
-        String[] values = new String[] { "Les misérables",
-                "rapport",
-                "pfe",
-                "livreSanté",
-                "livreFinance",
-                "LivreInformatique",
-                "LivreComptabilité",
-                "livreChimie"
-        };
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                int itemPosition     = position;
-
-                String  itemValue    = (String) listView.getItemAtPosition(position);
-
-                switch (itemPosition){
-                    case 0:
-                        Intent intent = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intent);
-                        break;
-                    case 1:
-                        Intent inten = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(inten);
-                        break;
-                    case 2:
-                        Intent intentt = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intentt);
-                        break;
-                    case 3:
-                        Intent intenttz = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intenttz);
-                        break;
-                    case 4:
-                        Intent intenttrr = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intenttrr);
-                        break;
-                    case 5:
-                        Intent intenttr = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intenttr);
-                        break;
-                    case 6:
-                        Intent intentte = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intentte);
-                        break;
-                    case 7:
-                        Intent intenttt = new Intent(DocInterface.this, infoDocument.class);
-                        startActivity(intenttt);
-                        break;
-                }
-
-
-
-            }
-
-        });
+        BackgroundTask2 backgroundTask2 = new BackgroundTask2();
+        backgroundTask2.execute();
     }
-
 }
