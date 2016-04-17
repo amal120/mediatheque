00391cda8b9288ea9c85ed1007d4ce613ffaa4a0
amal120 <amal_zine_el_abidine@yahoo.fr>
2016-04-17 20:30:11 +0100

    Update Place.java

--- app/src/main/java/com/example/dell/mediatheque/Place.java
+++ app/src/main/java/com/example/dell/mediatheque/Place.java
@@ -7,18 +7,38 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Date;
 
 public class Place extends AppCompatActivity {
-
+    EditText etMat2;
+    DatePicker etRes1;
+    TimePicker etHeur1;
+    int heureRes;
+    String mat_etud;
+    String dateRes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place);
+        etMat2 = (EditText) findViewById(R.id.etMatt);
+        etRes1 = (DatePicker) findViewById(R.id.etRes);
+        etHeur1 = (TimePicker) findViewById(R.id.etHeur);
 
     }
-    public void userPlac (View view)
+    public void userP(View view)
     {
-        startActivity(new Intent(this, Accueil.class));
-
+        mat_etud  = etMat2.getText().toString();
+        dateRes  = etRes1.getCalendarView().toString();
+        heureRes  = etHeur1.getHour();
+        String method = "reservplac";
+      BackgroundTask3 backgroundTask3 = new BackgroundTask3(this);
+        backgroundTask3.execute(method,mat_etud,dateRes, String.valueOf(heureRes));
+        finish();
     }
+
 }
