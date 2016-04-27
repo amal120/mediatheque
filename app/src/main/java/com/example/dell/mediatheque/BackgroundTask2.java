633c7115c055d37c82e91082248d853f285ae9f4
amal120 <amal_zine_el_abidine@yahoo.fr>
2016-04-27 21:50:51 +0000

    Update BackgroundTask2.java

--- app/src/main/java/com/example/dell/mediatheque/BackgroundTask2.java
+++ app/src/main/java/com/example/dell/mediatheque/BackgroundTask2.java
@@ -1,106 +1,89 @@
 package com.example.dell.mediatheque;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.net.URLEncoder;
 
 /**
- * Created by DELL on 16/04/2016.
+ * Created by DELL on 07/04/2016.
  */
-public class BackgroundTask2 extends AsyncTask<Void,Document,Void> {
-    String json_string = "http://192.168.124.1/media/getdoc.php";
+public class BackgroundTask2 extends AsyncTask <String,Void,String> {
     Context ctx;
-    Activity activity;
-    RecyclerView recyclerView;
-    RecyclerView.Adapter adapter;
-    RecyclerView.LayoutManager layoutManager;
-    ProgressDialog progressDialog;
-    ArrayList<Document> arrayList = new ArrayList<>();
 
+    BackgroundTask2(Context ctx) {
+        this.ctx = ctx;
+    }
 
     @Override
     protected void onPreExecute() {
-        recyclerView = (RecyclerView)activity.findViewById(R.id.recyclerView);
-        layoutManager = new LinearLayoutManager(ctx);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-        adapter = new RecyclerAdapter(arrayList);
-        recyclerView.setAdapter(adapter);
-        progressDialog = new ProgressDialog(ctx);
-        progressDialog.setTitle("Please wait..");
-        progressDialog.setMessage("List is loading..");
-        progressDialog.setIndeterminate(true);
-        progressDialog.setCancelable(false);
-        progressDialog.show();
+
+        super.onPreExecute();
     }
 
     @Override
-    protected Void doInBackground(Void... params) {
-        try {
-            URL url = new URL(json_string);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            while ((line=bufferedReader.readLine()) !=null)
-            {
-                stringBuilder.append(line+"\n");
+    protected String doInBackground(String... params) {
+        String doc_url = "http://192.168.124.1/media/doc.php";
+        String method = params[0];
+        if (params != null && method.equals("resdoc"))
+
+        {
+            String matriculet = params[1];
+            String dateEmprunt = params[2];
+            String titreD = params[3];
+            try {
+                URL url = new URL(doc_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String data = URLEncoder.encode("matriculet", "UTF-8") + "=" + URLEncoder.encode(matriculet, "UTF-8") + "&" +
+                        URLEncoder.encode("dateEmprunt", "UTF-8") + "=" + URLEncoder.encode(dateEmprunt, "UTF-8") + "&" +
+                        URLEncoder.encode("titreD", "UTF-8") + "=" + URLEncoder.encode(titreD, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream IS = httpURLConnection.getInputStream();
+                IS.close();
+                httpURLConnection.disconnect();
+                return "Reservation Success...";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            httpURLConnection.disconnect();
-            String json_string = stringBuilder.toString().trim();
-            JSONObject jsonObject = new JSONObject(json_string);
-            JSONArray jsonArray = jsonObject.getJSONArray("server_response");
-            int count = 0;
-            while (count<jsonArray.length()) {
-
-                JSONObject JO = jsonArray.getJSONObject(count);
-                count++;
-                Document document = new Document(JO.getString("code"),JO.getString("titre"),JO.getString("auteur"),JO.getString("type"));
-                publishProgress(document);
-                Thread.sleep(1000);
-            }
-            Log.d("JSON STRING", json_string);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         }
-        catch (IOException e){
-            e.printStackTrace();
-        }  catch (JSONException e) {
-            e.printStackTrace();
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         return null;
     }
 
-    @Override
-    protected void onProgressUpdate(Document... values) {
-            arrayList.add(values[0]);
-            adapter.notifyDataSetChanged();    }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        progressDialog.dismiss();
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
     }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+
+    }
+
 }
