cb67a319780994b08728e8784b3192e0d842babb
amal120 <amal_zine_el_abidine@yahoo.fr>
2016-04-27 21:54:32 +0000

    Update DocInterface.java

--- app/src/main/java/com/example/dell/mediatheque/DocInterface.java
+++ app/src/main/java/com/example/dell/mediatheque/DocInterface.java
@@ -1,24 +1,38 @@
 package com.example.dell.mediatheque;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class DocInterface extends AppCompatActivity {
+    ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document);
-        BackgroundTask2 backgroundTask2 = new BackgroundTask2();
-        backgroundTask2.execute();
+        listView = (ListView) findViewById(R.id.list);
+
+        String[] values = new String[] { "Les misérables",
+                "rapport",
+                "pfe",
+                "livreSanté",
+                "livreFinance",
+                "LivreInformatique",
+                "LivreComptabilité",
+                "livreChimie"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        listView.setAdapter(adapter);
+
+
+
+            }
+
     }
-}
+
