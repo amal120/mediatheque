a772b08d16c7b2f801118d4947ed7c89fba04542
amal120 <amal_zine_el_abidine@yahoo.fr>
2016-04-27 21:55:39 +0000

    Update Place.java

--- app/src/main/java/com/example/dell/mediatheque/Place.java
+++ app/src/main/java/com/example/dell/mediatheque/Place.java
@@ -2,23 +2,15 @@ package com.example.dell.mediatheque;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TimePicker;
-
-import java.util.Date;
 
 public class Place extends AppCompatActivity {
     EditText etMat2;
-    DatePicker etRes1;
-    TimePicker etHeur1;
-    int heureRes;
+    EditText etRes1;
+    EditText etHeur1;
+    String heureRes;
     String mat_etud;
     String dateRes;
     @Override
@@ -26,19 +18,23 @@ public class Place extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place);
         etMat2 = (EditText) findViewById(R.id.etMatt);
-        etRes1 = (DatePicker) findViewById(R.id.etRes);
-        etHeur1 = (TimePicker) findViewById(R.id.etHeur);
+        etRes1 = (EditText) findViewById(R.id.etRes);
+        etHeur1 = (EditText) findViewById(R.id.etHeur);
 
     }
     public void userP(View view)
     {
         mat_etud  = etMat2.getText().toString();
-        dateRes  = etRes1.getCalendarView().toString();
-        heureRes  = etHeur1.getHour();
+        dateRes  = etRes1.getText().toString();
+        heureRes  = etHeur1.getText().toString();
         String method = "reservplac";
       BackgroundTask3 backgroundTask3 = new BackgroundTask3(this);
-        backgroundTask3.execute(method,mat_etud,dateRes, String.valueOf(heureRes));
+        backgroundTask3.execute(method,dateRes,heureRes,mat_etud);
         finish();
     }
+    public void userPlac (View view)
+    {
+        startActivity(new Intent(this, Mediatheque.class));
 
+    }
 }
